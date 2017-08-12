33c7d25ecfe4e546503485120732faf742f4900b
Samir Dhebar <samirdhebar@gmail.com>
2017-08-12 11:35:35 -0400

    amended requireLoggedIn mware to redirect to photo/gallery

--- middleware/requireLoggedIn.js
+++ middleware/requireLoggedIn.js
@@ -1,10 +1,10 @@
-function requireLoggedIn(req, res, next) {
-        if (req.user) {
-                  next();
-        }
-        else {
-                res.redirect("/");
-        }
+function requireLoggedIn (req, res, next) {
+		if (req.user) {
+			next();
+		}
+		else {
+			res.redirect("/photo/gallery");
+		}
 }
 
 module.exports = requireLoggedIn;
