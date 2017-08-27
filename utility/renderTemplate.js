a66a2c6c1c95628825e40801863eb8109406142f
Samir Dhebar <samirdhebar@gmail.com>
2017-08-26 21:15:21 -0400

    cleaned up unnecessary code

--- utility/renderTemplate.js
+++ utility/renderTemplate.js
@@ -1,7 +1,7 @@
 function renderTemplate(res, title, page, args, links, error) {
 	return res.render("template", {
 		page: page,
-		title: title || "Hey Mom",
+		title: title
 		args: args,
 	});
 }
